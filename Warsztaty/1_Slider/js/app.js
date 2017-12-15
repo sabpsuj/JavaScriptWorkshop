a190b934002c116dc6638054c3d20342ed3e22f3
sabpsuj <sabinapsuj@gmail.com>
2017-12-15 15:23:12 +0100

    Prosty Slider Js

--- Warsztaty/1_Slider/js/app.js
+++ Warsztaty/1_Slider/js/app.js
@@ -1,3 +1,32 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener("DOMContentLoaded", function () {
+    
+        var next = document.getElementById('nextPicture');
+        var prev = document.getElementById('prevPicture');
+        var slider = document.querySelectorAll('li.slider');
+        var index = 0;
+    
+        console.log(next);
+        console.log(prev);
+        console.log(slider);
+        console.log(index);
+    
+        slider[index].classList.add('visible');
+    
+        next.addEventListener('click', function(){
+            slider[index].classList.remove('visible');
+            index++;
+            if(index>=slider.length){
+                index = slider.length -1;
+            }
+            slider[index].classList.add('visible');
+        })
+        prev.addEventListener('click', function(){
+            slider[index].classList.remove('visible');
+            index--;
+            if(index<0){
+                index = index + 1;
+            }
+            slider[index].classList.add('visible');
+        })
+    
+    })

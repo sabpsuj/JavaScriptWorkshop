dcf3bf653f292c1982d465d15fa720849951c9b0
sabpsuj <sabinapsuj@gmail.com>
2017-12-28 17:29:43 +0100

    Simple menu

--- Warsztaty/4_Menu/js/app.js
+++ Warsztaty/4_Menu/js/app.js
@@ -1,3 +1,24 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener("DOMContentLoaded", function () {
+    console.log('Działa!');
+
+    var mainList = document.querySelectorAll('.nav>ul>li');
+    console.log(mainList);
+
+    for(var i=0; i<mainList.length; i++){
+        mainList[i].addEventListener('mouseover', function (event) {
+            if(this.children.length > null){
+                this.firstElementChild.style.display = "block";
+            }
+        });
+        mainList[i].addEventListener('mouseout', function (event) {
+            if(this.children.length > null){
+                this.firstElementChild.style.display = "none";
+            }
+        });
+    };
+
+
+
+
+
+});

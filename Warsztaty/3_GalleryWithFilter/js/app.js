231e773425d2f847aa552b8ac1ac49e05317ce90
sabpsuj <sabinapsuj@gmail.com>
2017-12-28 16:02:45 +0100

    Gallery with tag filter

--- Warsztaty/3_GalleryWithFilter/js/app.js
+++ Warsztaty/3_GalleryWithFilter/js/app.js
@@ -1,3 +1,43 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener("DOMContentLoaded", function () {
+
+    var images = document.querySelectorAll('img');
+    var hideButton = document.getElementById('hideButton');
+    var showButton = document.getElementById('showButton');
+    var tagInput = document.getElementById('tagInput');
+
+    console.log(images);
+    console.log(hideButton);
+    console.log(showButton);
+    console.log(tagInput);
+
+    showButton.addEventListener('click', function (e) {
+        var placeholderText = tagInput.value;
+        images.forEach(function (element) {
+            var arr = element.getAttribute('data-tag');
+            var tagArr = arr.split(',');
+            for (i = 0; i < tagArr.length; i++)
+                if (placeholderText === tagArr[i]) {
+                element.classList.remove('invisible');
+            }else{
+                element.classList.add('invisible');
+                }
+        });
+    });
+
+    hideButton.addEventListener('click', function (e) {
+        var placeholderText = tagInput.value;
+        images.forEach(function (element) {
+            var arr = element.getAttribute('data-tag');
+            var tagArr = arr.split(',');
+            for (i = 0; i < tagArr.length; i++) {
+                if (placeholderText === tagArr[i]) {
+                    element.classList.add('invisible');
+                }else{
+                    element.classList.remove('invisible');
+                }
+            }
+        });
+    });
+
+});
+

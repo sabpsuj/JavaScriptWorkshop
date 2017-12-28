5122957272c82856372b844e17622cf90177c70f
sabpsuj <sabinapsuj@gmail.com>
2017-12-28 18:34:58 +0100

    Tooltips Js

--- Warsztaty/5_Tooltips/js/app.js
+++ Warsztaty/5_Tooltips/js/app.js
@@ -1,8 +1,36 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
-
-
 /*
  <span class="tooltipText">Text tooltipa</span>
  */
+document.addEventListener("DOMContentLoaded", function () {
+    var tooltip1 = document.querySelector('span.tooltip:first-of-type');
+    var tooltip2 = document.querySelector('span.tooltip:nth-of-type(2)');
+    var tooltip3 = document.querySelector('span.tooltip:last-of-type');
+    var tooltipText = document.createElement('span');
+
+    tooltip1.addEventListener('mouseover', function (event) {
+        tooltip1.appendChild(tooltipText);
+        tooltipText.innerText = tooltip1.dataset.text;
+        tooltipText.classList.add('tooltipText');
+    });
+    tooltip1.addEventListener('mouseout', function (event) {
+        tooltip1.removeChild(tooltipText);
+    });
+
+    tooltip2.addEventListener('mouseover', function (event) {
+        tooltip2.appendChild(tooltipText);
+        tooltipText.innerText = tooltip2.dataset.text;
+        tooltipText.classList.add('tooltipText');
+    });
+    tooltip2.addEventListener('mouseout', function (event) {
+        tooltip2.removeChild(tooltipText);
+    });
+
+    tooltip3.addEventListener('mouseover', function (event) {
+        tooltip3.appendChild(tooltipText);
+        tooltipText.innerText = tooltip3.dataset.text;
+        tooltipText.classList.add('tooltipText');
+    });
+    tooltip3.addEventListener('mouseout', function (event) {
+        tooltip3.removeChild(tooltipText);
+    });
+});

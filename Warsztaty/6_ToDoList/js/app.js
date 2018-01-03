9d627df768ab623b6beb14a3c58913d8ad192d63
sabpsuj <sabinapsuj@gmail.com>
2018-01-03 12:30:12 +0100

    Korekty i ToDoList

--- Warsztaty/6_ToDoList/js/app.js
+++ Warsztaty/6_ToDoList/js/app.js
@@ -1,6 +1,59 @@
-/**
- * Created by Jacek on 2015-12-16.
- */
+document.addEventListener("DOMContentLoaded", function () {
+    var taskInput = document.querySelector('input');
+    var addButton = document.querySelector('#addTaskButton');
+    var tasksList = document.querySelector('ul');
+    var removeButton = document.querySelector('#removeFinishedTasksButton');
+    var counter = document.querySelector('span#counter');
+    var count = 0;
+    taskInput.value = taskInput.innerHTML;
+    var arr = [];
+
+    function addTask() {
+        if (taskInput.value.length >= 5 && taskInput.value.length <= 100) {
+            var li = document.createElement('li');
+            var taskTitle = document.createElement('h1');
+            var completeButton = document.createElement('button');
+            var deleteButton = document.createElement('button');
+            tasksList.appendChild(li);
+            li.appendChild(taskTitle);
+            taskTitle.innerText = taskInput.value;
+            li.appendChild(deleteButton);
+            deleteButton.innerText = 'Delete';
+            li.appendChild(completeButton);
+            completeButton.innerText = 'Complete';
+            taskInput.value = '';
+            arr.push(li);
+            counter.innerText = ++count;
+        }
+    };
+
+    tasksList.onclick = function (event) {
+        var target = event.target;
+        if (target.innerText === 'Complete') {
+            if (target.parentElement.classList !== 'done') {
+                target.parentElement.classList.toggle('done');
+                counter.innerText = --count;
+
+            }
+        } else if (target.innerText === 'Delete') {
+            target.parentElement.parentElement.removeChild(target.parentElement);
+            counter.innerText = --count;
+        }
+    }
+
+
+    function removeCompleted() {
+        var toDelete = document.querySelectorAll('li.done');
+        for (var i = 0; i < toDelete.length; i++) {
+                toDelete[i].parentElement.removeChild(toDelete[i]);
+        }
+    };
+
+
+    addButton.addEventListener('click', addTask);
+    removeButton.addEventListener('click', removeCompleted);
+})
+;
 
 
 
